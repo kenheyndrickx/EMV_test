3088d8ac343fe23df13608224892b60c8a335b40
Ken Heyndrickx <kenheyndrickx@gmail.com>
2016-08-15 09:43:12 +0200

    Read from csv

---
+++ NFC.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import scipy.signal
+import matplotlib.pyplot as mp
+import matplotlib.mlab as mm
+#import csv
+
+df = pd.read_csv('/home/ken/git/EMV_test/TypeA_data.csv')
+df.head()
